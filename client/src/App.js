1b579455b4bc13ee5e43223bd28074c9aa79a361
Mathias Thibaud <math.thibaud@gmail.com>
2020-04-17 17:08:21 -0700

    Used Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
 import './App.css';
 
-class App extends Component {
+function App() {
   // Initialize state
-  state = { passwords: [] }
+  const [passwords, setPasswords] = useState([]);
+  const [trigger, setTrigger] = useState(false);
 
   // Fetch passwords after first mount
-  componentDidMount() {
-    this.getPasswords();
-  }
-
-  getPasswords = () => {
-    // Get the passwords and store them in state
+  useEffect(() => {
     fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
-  }
+      .then((res) => res.json())
+      .then((data) => setPasswords(data));
+  }, [trigger]);
 
-  render() {
-    const { passwords } = this.state;
-
-    return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {passwords.length ? (
+        <div>
+          <h1>5 Passwords.</h1>
+          <ul className="passwords">
+            {passwords.map((password, index) => (
+              // Generally not OK to use 'index' as a key.
+              // But OK here because there will be the same # of passwords,
+              // and they never change positions in the array.
+              <li key={index}>
+                {password}
+              </li>
+            ))}
+          </ul>
+          <button
+            className="more"
+            onClick={() => setTrigger(!trigger)}
+            type="submit"
+          >
+            Get More
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1>No passwords :(</h1>
+          <button
+            className="more"
+            onClick={() => setTrigger(!trigger)}
+            type="submit"
+          >
+            Try Again?
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
